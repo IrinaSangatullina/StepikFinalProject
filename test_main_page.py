f9773d35c1afddc7267a1781f553cacee05bb9a1
Irina <1995.kis@mail.ru>
2023-07-06 18:09:25 +0300

    Реализация LoginPage и переход между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,18 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
